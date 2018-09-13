b34c41d0202333e44802f9cb9657626b6dc36b51
Lyndros <lyndros@hotmail.com>
2018-09-13 19:34:14 +0200

    Beta 2 version.

---
+++ README.md
@@ -0,0 +1,64 @@
+<html>
+<body style="font-family: Consolas, monospace; font-size:14pt;">
+<b>Masternode installer instructions</b>
+<br/> ────────────────────────────────────────────────────
+<br/>
+<br/> BROKEN BUILD WORK IN PROGRESS... SORRY
+<br/>
+<br/> A beautiful python installer that allows you to install your beloved masternodes withing minutes.
+<br/> 
+<br/> NOTE: Currently the installer ONLY supports TOKUGAWA coin, more coins will be added.
+<br/>
+<br/> <b>0. Requirements</b>
+<br/>
+<br/> In order to run this program the below python3 libraries need to be installed:
+<br/> &nbsp; &nbsp; $sudo apt-get install python3-pip python3-yaml
+<br/> Additionally ufw firewall must be installed:
+<br/> $sudo apt install ufw
+<br/> 
+<br/> In addition the following files are needed:
+<br/> &nbsp; &nbsp; - A precompiled binary containing the latest version of the masternode daemon 
+<br/> &nbsp; &nbsp;   NOTE: that the binary dependant libraries must be pre-installed (Work on progress).
+<br/> &nbsp; &nbsp; - Ufw firewall will be automatically configured to enable SSH/Tokugawa ports. ($apt install ufw).
+<br/> &nbsp; &nbsp; - [Optional] A bootstrap file to accelerate the initial masternode synchronisation.
+<br/>
+<br/> <b>1. Copying the necessary files</b>
+<br/>
+<br/> &nbsp; &nbsp; $mkdir -p /tmp/masternode_installer/
+<br/> &nbsp; &nbsp; $cd /tmp/masternode_installer/
+<br/> &nbsp; &nbsp; $wget https://raw.githubusercontent.com/Lyndros/crypto_tools/master/masternode_installer/masternode_installer.py
+<br/>
+<br/> -- This is a configuration example, please modify as needed before running the installer--
+<br/> &nbsp; &nbsp; $wget https://raw.githubusercontent.com/Lyndros/crypto_tools/master/masternode_installer/tokugawa_config.yml
+<br/>
+<br/> &nbsp; &nbsp; In addition get your tokugawad binary from your favourite source or compile it.
+<br/> &nbsp; &nbsp; A bootstrap.dat file is optional but very recommended if you want to have your MNs running asap.
+<br/>
+<br/> <b>2. Setting your configuration file</b>
+<br/> 
+<br/> Modify the configuration file to match your needs: mainly IP, port and masternode privkey settings...
+<br/> If you run multiple masternodes in the same VPS, you can share the IP, take into account that ports must be different.
+<br/>
+<br/> <b>3. Running the installer</b>
+<br/> &nbsp; &nbsp; $./masternode_installer.py installation_directory tokugawad_executable configuration.yml [ --bootstrap bootstrap.dat ]
+<br/>
+<br/> &nbsp; &nbsp; Execution examples:
+<br/> &nbsp; &nbsp; $./masternode_installer.py /opt/tokugawa /tmp/tokugawad /tmp/tokugawa_config.yml --bootstrap /tmp/bootstrap.dat
+<br/> &nbsp; &nbsp; $./masternode_installer.py /opt/tokugawa /tmp/tokugawad /tmp/tokugawa_config.yml
+<br/>
+<br/> <b>4. Enabling tokugawa services automatically at boot</b>
+<br/> &nbsp; &nbsp; $systemctl enable tokugawa_mn*
+<br/> 
+<br/> If during the installation ufw firewall was detected the following steps must be done:
+<br/> &nbsp; &nbsp; $ufw stop; 
+<br/> &nbsp; &nbsp; $ufw allow tokugawa_mn*; $ufw enable; 
+<br/> &nbsp; &nbsp; $systemctl enable ufw;
+<br/>
+<br/> <b>5. Donations</b>
+<br/> If you want to motivate me and support this repository I accept donations even 1 TOK is always welcome :-)!
+<br/> &nbsp; &nbsp;> <b>ethereum address:</b> <i>0x44F102616C8e19fF3FED10c0b05B3d23595211ce</i>
+<br/> &nbsp; &nbsp;> <b>tokugawa address:</b> <i>TjrQBaaCPoVW9mPAZHPfVx9JJCq7yZ7QnA</i>
+<br/>
+<br/> For any questions feel free to contact me at <i>lyndros@hotmail.com</i>
+</body>
+</html>
---
+++ masternode_installer.py
@@ -0,0 +1,308 @@
+#!/usr/bin/env python3
+# Copyright (c) 2018 Lyndros <lyndros@hotmail.com>
+# Distributed under the MIT/X11 software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+###############################################################################
+# MASTERNODE AUTOINSTALLER by Lyndros <lyndros@hotmail.com>                   #
+###############################################################################
+# Repository: https://github.com/Lyndros/crypto_tools                         #
+#                                                                             #
+# If you want to support and motivate me I accept donations even 1 TOK is     #
+# always welcome :-)!                                                         #
+# > ethereum address: 0x44F102616C8e19fF3FED10c0b05B3d23595211ce              #
+# > tokugawa address: TjrQBaaCPoVW9mPAZHPfVx9JJCq7yZ7QnA                      #
+#                                                                             #
+###############################################################################
+import yaml
+import argparse
+import sys
+import os
+import errno
+import string
+import random
+import subprocess
+from shutil import copyfile 
+
+#List of supported coins by the installer
+supported_coins = ['Tokugawa']
+
+def show_banner():
+    print('')
+    print('###############################################################################')
+    print('# MASTERNODE AUTOINSTALLER by Lyndros <lyndros@hotmail.com>                   #')
+    print('###############################################################################')
+    print('# Repository: https://github.com/Lyndros/crypto_tools                         #')
+    print('#                                                                             #')
+    print('# If you want to support and motivate me I accept donations even 1 TOK is     #')
+    print('# always welcome :-)!                                                         #')
+    print('# > ethereum address: 0x44F102616C8e19fF3FED10c0b05B3d23595211ce              #')
+    print('# > tokugawa address: TjrQBaaCPoVW9mPAZHPfVx9JJCq7yZ7QnA                      #')
+    print('#                                                                             #')
+    print('###############################################################################')
+    print('')
+
+def show_warning(os_name):
+    print('This installer is configured to run on %s' %os_name)
+    input('Press ENTER to continue, or CTRL+C to abort installation.\n')
+
+#Function to generate a random password
+#Note: Passwords are enforced to be 12 characters long
+def generate_password():
+    min_char = 12
+    max_char = 12
+    allchar = string.ascii_letters + string.punctuation + string.digits
+    password = "".join(random.choice(allchar) for x in range(random.randint(min_char, max_char)))
+    
+    return password
+
+#Function to create a directory
+#Note: All non existing parent directories will be created
+def create_directory(absfolder_path):
+    try:
+        os.makedirs(absfolder_path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+#Function to generate the tokugawa service
+def generate_init_service(filename_abspath, name, start_command, stop_command):
+    with open(filename_abspath, 'w') as config:
+        config.write('#! /bin/sh\n');
+        config.write('### BEGIN INIT INFO\n');
+        config.write("# Provides:          %s\n" %name);
+        config.write('# Required-Start:\n');
+        config.write('# Required-Stop:\n');
+        config.write('# Default-Start:     2 3 4 5\n');
+        config.write('# Default-Stop:      0 1 6\n');
+        config.write("# Short-Description: %s masternode service\n" %name);
+        config.write('### END INIT INFO\n');
+        config.write('\n')
+        config.write("PIDOF_PROG=/bin/pidof\n");
+        config.write('\n')
+        config.write('case "$1" in\n');
+        config.write('  start)\n');
+        config.write("    echo \"Starting %s\";\n" %name);
+        config.write("    sudo -u root %s;\n" %start_command);
+        config.write('  ;;\n');
+        config.write('  restart|reload|force-reload)\n');
+        config.write('    echo "Error: argument \'$1\' not supported" >&2\n');
+        config.write('    exit 3\n');
+        config.write('  ;;\n');
+        config.write('  status)\n');
+        config.write("    PROG_PID=`sudo -u root ${PIDOF_PROG} %s`;\n" %start_command);
+        config.write('    if [ $? -eq 0 ]; then\n');
+        config.write("      echo \"%s is running with pid ${PROG_PID}\"\n" %name);
+        config.write('    else\n');
+        config.write("      echo \"%s is not running\"\n" %name);
+        config.write('    fi\n');
+        config.write('  ;;\n');
+        config.write('  stop)\n');
+        config.write("    PROG_PID=`sudo -u root ${PIDOF_PROG} %s`;\n" %start_command);
+        config.write("    echo \"Stopping %s\";\n" %name);
+        config.write("    sudo -u root %s;\n" %stop_command);
+        config.write('  ;;\n');
+        config.write('  *)\n');
+        config.write('    echo "Usage: $0 start|stop" >&2\n');
+        config.write('    exit 3\n');
+        config.write('  ;;\n');
+        config.write('esac\n');
+
+    #Set access rights to file
+    os.chmod(filename_abspath, 0o755)
+
+#Function to generate an UFW application profile
+def generate_ufw_profile(filename_abspath, name, title, description, ports, protocols):
+    #Write application profile
+    with open(filename_abspath, 'w') as config:
+        config.write("[%s]\n"           %name)
+        config.write("title=%s\n"       %title)
+        config.write("description=%s\n" %description)
+        #Build string for ports
+        str_ports     = "".join("%s," %p for p in ports)
+        str_protocols = "".join("%s/" %p for p in protocols)
+        #Add ports and protocols removing trainling char
+        config.write("ports=%s/%s\n"    %(str_ports[:-1], str_protocols[:-1]))
+
+    #Set access rights to file
+    os.chmod(filename_abspath, 0o644)
+
+# Function to generate the Tokugawa.conf file under the MN directory
+def generate_masternode_tokugawaconf(filename_abspath, masternode_name, rcpport, ip, port, privkey):
+    with open(filename_abspath, 'w+') as config:
+        config.write('rpcuser=myrpcuser%s\n' %masternode_name.lower())
+        config.write('rpcpassword=%s\n' % generate_password())
+        config.write('rpcport=%s\n' %rcpport)
+        config.write('server=1\n')
+        config.write('listen=1\n')
+        config.write('daemon=1\n')
+        config.write('port=%s\n' %port)
+        config.write('masternodeaddr=%s:%s\n' % (ip, port))
+        config.write('masternode=1\n')
+        config.write('masternodeprivkey=%s\n' % mn['privkey'])
+
+    #Set access rights to file
+    os.chmod(filename_abspath, 0o644)
+
+#Function to install binaries in the desire location
+def install_masternode_binaries(executable_abspath, masternode_executable_abspath):
+    #Copy daemon file with the following name
+    copyfile(executable_abspath, masternode_executable_abspath)
+    #Set execution rights to file
+    os.chmod(masternode_executable_abspath, 0o755)
+
+#Function to deploy a masternode configuration in the desire location
+def deploy_masternode_configuration(coinname, mn, installdir_abspath):
+    #Masternode specific configuration
+    if coinname == 'Tokugawa':
+        #Tokugawa.conf file generation
+        masternode_tokugawaconf_abspath = os.path.abspath(masternodedir_abspath +'/Tokugawa.conf')
+        generate_masternode_tokugawaconf(masternode_tokugawaconf_abspath, mn['name'], mn['rpcport'], mn['ip'], mn['ports'][0], mn['privkey'])
+        #Set access rights to file
+        os.chmod(masternode_tokugawaconf_abspath, 0o644)
+
+
+#Function to deploy a masternode configuration in the desire location
+def deploy_masternode_bootstrap(coinname, bootstrap_abspath, masternodedir_abspath):
+    #Copy bootstrap if present
+    if coinname == 'Tokugawa':
+        masternode_bootstrap_basename = os.path.basename(bootstrap_abspath)
+        masternode_bootstrap_abspath  = os.path.abspath(masternodedir_abspath+'/'+masternode_bootstrap_basename)
+        copyfile(bootstrap_abspath, masternode_bootstrap_abspath)
+        #Set access rights to file
+        os.chmod(masternode_bootstrap_abspath, 0o644)
+
+#Function that returns start command depending on coin name
+def get_masternode_start_command(coinname, masternode_executable_abspath, masternodedir_abspath):
+    if coinname == 'Tokugawa':
+        start_command = masternode_executable_abspath + " --datadir=%s" %masternodedir_abspath
+
+    return start_command
+
+#Function that returns stop command depending on coin name
+def get_masternode_stop_command(coinname, masternode_executable_abspath):
+    if coinname=='Tokugawa':
+        stop_command = masternode_executable_abspath + " stop"
+
+    return stop_command
+
+def package_install(package):
+    cmd = '/usr/bin/apt-get install'.split()
+    cmd.append(package)
+    output_kw = {'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE}
+    p = subprocess.Popen(cmd, **output_kw)
+    status = p.wait()
+    error = p.stderr.read().lower()
+    if status and 'permission denied' in error:
+        raise OSError(errno.EACCES, 'Permission denied running apt-get')
+
+def run_command(command):
+    # Launch the command with pexpect need to send the command join reverse the shlex
+    #command_child = subprocess.list2cmdline(self.command)
+    try:
+        subprocess.call(command, shell=True)
+    except OSError as e:
+        print ("Error executing command %s" %command)
+
+
+###############################################################################
+#                                    MAIN                                     #
+###############################################################################
+
+#Chek user rights
+if os.geteuid() != 0:
+    exit("You need to have root privileges to run this script.")
+
+#Program input parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("installdir",    help="The installation directory.")
+parser.add_argument("executable",    help="The tokugawad binary file.")
+parser.add_argument("configuration", help="The installer configuration file.")
+parser.add_argument("--bootstrap",   help="The bootstrap file.")
+args = parser.parse_args()
+
+#Build abs names to avoid problems
+installdir_abspath    = os.path.abspath(args.installdir)
+executable_abspath    = os.path.abspath(args.executable)
+configuration_abspath = os.path.abspath(args.configuration)
+
+if args.bootstrap:
+    bootstrap_abspath = os.path.abspath(args.bootstrap)
+else:
+    bootstrap_abspath = ""
+
+#Check input files
+if (not os.path.exists(executable_abspath)) or (not os.path.exists(configuration_abspath)):
+    print("Error! One of the supplied files does not exist.")
+    sys.exit(-1)
+
+#Parse the configuration file
+with open(configuration_abspath, 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+if cfg['coinname'] not in supported_coins:
+    print('Sorry the selected coin is not yet supported!')
+    pritn('List of supported coins: %s' %supported_coins)
+
+#Show presentation
+show_banner()
+show_warning(cfg['SYSTEM']['description'])
+
+print('[PREREQUISITES]')
+print('  >> Installing required packages [WORKING ON PROGRESS]')
+
+if cfg['test']=='enabled':
+    exit("Testing packages finish")
+
+
+print('[INSTALLATION START]')
+for mn in cfg['MASTERNODES']:
+    #Masternode configuration parameters
+    masternode_name                = "%s_%s".capitalize() %(cfg['coinname'], mn['name'])
+    masternodedir_abspath          = os.path.abspath(installdir_abspath + "/." + mn['name'])
+    masternode_executable_basename = os.path.basename(executable_abspath + "_" + mn['name'])
+    masternode_executable_abspath  = os.path.abspath(installdir_abspath + '/' + masternode_executable_basename)
+    ufwprofile_abspath             = "/tmp/etc/ufw/applications.d/%s" %masternode_name.lower()
+    servicefile_abspath            = "/tmp/etc/init.d/%s"             %masternode_name.lower()
+    #Print masternode name
+    print("  %s" %masternode_name)
+    #Create installation directory
+    print('    >> Creating installation directory')
+    create_directory(installdir_abspath)
+    #Install masternode software
+    print('    >> Installing binaries')
+    install_masternode_binaries(executable_abspath, masternode_executable_abspath)
+    #Masternode directory creation
+    create_directory(masternodedir_abspath)
+    #Masternode configuration deployment
+    print('    >> Deploying configuration')
+    deploy_masternode_configuration(cfg['coinname'], mn, masternodedir_abspath)
+    #Masternode bootstrap deployment
+    if bootstrap_abspath:
+        print('    >> Deploying bootstrap')
+        deploy_masternode_bootstrap(cfg['coinname'], bootstrap_abspath, masternodedir_abspath)
+    #Generating boot service
+    print('    >> Generating /etc/init.d service')
+    #Masternode specific startup commands
+    start_command = get_masternode_start_command(cfg['coinname'], masternode_executable_abspath, masternodedir_abspath)
+    stop_command  = get_masternode_stop_command(cfg['coinname'], masternode_executable_abspath)
+    generate_init_service(servicefile_abspath, masternode_name, start_command, stop_command)
+    # Enable boot service
+    if cfg['services'] == 'enabled':
+        print(" >> Enabling boot service for %s" %masternode_name)
+        run_command("systemctl enable %s" %masternode_name)
+    #Generating firewall profile
+    print('    >> Generating UFW firewall profile')
+    generate_ufw_profile(ufwprofile_abspath, masternode_name, masternode_name, "Provides %s masternode service" %masternode_name, mn['ports'], mn['protocols'])
+    # Allow firewall profile
+    if cfg['firewall'] == 'enabled':
+        print(" >> Allow %s firewall profile" %masternode_name)
+        run_command("ufw allow %s" %masternode_name)
+
+print('[INSTALLATION FINISH]')
+print('')
+
+#Enable firewall
+if cfg['firewall'] == 'enabled':
+    print('To activate the firewall please run as root:')
+    print('\'$ufw stop; ufw enable; ufw start\'')
